ff80409a966adc83a033868a927536ab942a6262
CarolineCunha <caroline.cunha.b@hotmail.com>
2020-10-24 19:05:33 -0300

    fim do desafio

---
+++ src/database/migrations/1603555370567-CreateTransactions.ts
@@ -0,0 +1,56 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+import Category from "../../models/Category";
+
+export default class CreateTransactions1603555370567 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+          new Table({
+            name: 'transactions',
+            columns: [
+              {
+                name: 'id',
+                type: 'uuid',
+                isPrimary: true,
+                generationStrategy: 'uuid',
+                default: 'uuid_generate_v4()',
+              },
+              {
+                name: 'title',
+                type: 'varchar',
+              },
+              {
+                name: 'type',
+                type: 'varchar',
+              },
+              {
+                name: 'value',
+                type: 'decimal',
+              },
+             // {
+              //  name: 'category_id',
+              //  type: 'uuid',
+             // },
+              {
+                name: 'created_at',
+                type: 'timestamp',
+                default: 'now()',
+              },
+              {
+                name: 'updated_at',
+                type: 'timestamp',
+                default: 'now()',
+              },
+            ],
+          }),
+        );
+    
+      }
+    
+      public async down(queryRunner: QueryRunner): Promise<void> {
+
+    
+        await queryRunner.dropTable('transactions');
+      }
+
+}
